6fb1c8019dd04f6d84f5e818ac55d4ab22be9dbf
devildeath1 <55554693+devildeath1@users.noreply.github.com>
2022-03-20 13:07:39 +0530

    main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+const timeload = () => {
+  
+  let temp = document.getElementById('temp');
+  temp.innerHTML = "&#xf2cb";
+  
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2ca";
+    temp.style.color = "#f8b627";
+  },1000);
+  
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c9";
+  },2000);
+  
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c8";
+  },3000);
+  
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c7";
+    temp.style.color = "#d63031";
+  },4000);
+  
+}
+
+timeload();
+
+setInterval(timeload, 5000)
